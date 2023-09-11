0f1d99aff226059f7e4b9f1b3facedf38f92d714
mattummal <jehmatts@gmail.com>
2023-09-11 16:20:16 +0200

    Updates

---
+++ precache-manifest.3deebd4460dc8c657e6d099aa068d14c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "943a656f088b341e6ba1",
+    "url": "/static/css/main.f9035741.chunk.css"
+  },
+  {
+    "revision": "943a656f088b341e6ba1",
+    "url": "/static/js/main.943a656f.chunk.js"
+  },
+  {
+    "revision": "c73cf94dadba11707111",
+    "url": "/static/js/1.c73cf94d.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "471a5630b607cb053fb53712f7f9b33f",
+    "url": "/index.html"
+  }
+];
